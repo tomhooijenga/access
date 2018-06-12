e3157f734075f68f1124df32e89807023f477f2b
Tom Hooijenga <tomhooijenga@gmail.com>
2018-06-12 21:16:01 +0200

    Add build dist

--- dist/index.js
+++ dist/index.js
@@ -17,4 +17,3 @@ require("./array");
 require("./object");
 
 var _access = _interopRequireDefault(require("./access"));
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztBQUFBOztBQUNBOztBQUVBIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0ICcuL2FycmF5JztcclxuaW1wb3J0ICcuL29iamVjdCc7XHJcblxyXG5leHBvcnQgeyBkZWZhdWx0IH0gZnJvbSAnLi9hY2Nlc3MnOyJdfQ==
