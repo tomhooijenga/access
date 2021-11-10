52e923bc1e8da2fab2671113c34e7f0dbdc2ab40
Tom <tom@vespertool.com>
2021-11-10 22:49:15 +0100

    Fix typescript definitions

--- access.d.ts
+++ access.d.ts
@@ -1,7 +1,9 @@
+/// <reference lib="es2015.iterable" />
+
 export const access: Handler & {
     register(type: Function, handler: Partial<Handler>): void;
     unregister(type: Function | undefined): void;
-}
+};
 
 export default access;
 
@@ -11,6 +13,7 @@ export type Handler = {
     has(obj: any, key: any): boolean;
     delete(obj: any, key: any): boolean;
     clear(obj: any, key: any): void;
+    size(obj: any): number;
     keys(obj: any): IterableIterator<any>
     values(obj: any): IterableIterator<any>
     entries(obj: any): IterableIterator<[any, any]>
@@ -18,4 +21,15 @@ export type Handler = {
 
 export const types: Map<Function | undefined, Partial<Handler>>;
 
-export function wrap(obj: any): Handler;
+export function wrap(obj: any): {
+    get(key: any): any;
+    set(key: any, value: any): any;
+    has(key: any): boolean;
+    delete(key: any): boolean;
+    clear(key: any): void;
+    readonly size: number;
+    keys(): IterableIterator<any>
+    values(): IterableIterator<any>
+    entries(): IterableIterator<[any, any]>
+    [Symbol.iterator]: IterableIterator<[any, any]>
+};
