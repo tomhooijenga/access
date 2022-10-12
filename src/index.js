4e22628f288a4c164e611af42ce8b57084a3e130
Tom <tomhooijenga@gmail.com>
2022-10-12 15:14:03 +0200

    1.0.17

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './handlers/multi-dict';
 import './handlers/object';
 import './handlers/storage';
 
+// eslint-disable-next-line no-restricted-exports
 export { default, default as access } from './access';
 export { default as wrap } from './wrap';
 export { default as types } from './types';
